6ac18c8ed6fbd482f15940ff3fc7f1c49ed69bfd
Guilherme Lourenco <guilherme@ampliato.com.br>
2018-08-22 13:01:40 -0300

    Create Interceptors

--- src/main/webapp/curso-angularJS/gui/dev.2.0.1/js/interceptors/timestampInterceptor.js
+++ src/main/webapp/curso-angularJS/gui/dev.2.0.1/js/interceptors/timestampInterceptor.js
@@ -4,7 +4,6 @@ angular.module("listaTelefonica").factory("timestampInterceptor", function () {
             var url = config.url;
             // if (url.indexOf('view') > -1 ) return config; <- verifica se a url interceptada contem view
             if (url.indexOf('view') > -1 ) return config;
-
             var timestamp = new Date().getTime();
             config.url = url + "?timestamp=" + timestamp;
             return config;
